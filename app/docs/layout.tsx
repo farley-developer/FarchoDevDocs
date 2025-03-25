2e8d4aa80fad4bcd8a79bf4298131b6cf9ac6121
farley-developer <farley.developer@gmail.com>
2025-03-25 07:46:46 -0500

    Corrige importaciones y actualiza la configuración de los layouts en los componentes de Home y Docs.

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,15 +1,17 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
-
+import type { ReactNode } from 'react';
+ 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions} sidebar={{
-      tabs: [
-        
-      ],
-    }}>
+    <DocsLayout
+      {...baseOptions}
+      // the position of navbar
+      nav={{ ...baseOptions.nav }}
+      // Removed tabMode as it is not a valid property
+      tree={source.pageTree}
+    >
       {children}
     </DocsLayout>
   );
